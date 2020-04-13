1736354e1d8a6263ae69f731047f65d8dee26651
Karlis Berzins <berzinsh.karlis@gmail.com>
2020-04-13 07:10:39 +0300

    Initial project

---
+++ appstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appstore
+//
+//  Created by karlis.berzins on 13/04/2020.
+//  Copyright © 2020 berzinsk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
